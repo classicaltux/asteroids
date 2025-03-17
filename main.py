c5e1e3f8b83a6341b3ca120af75d873adb80e68f
classicaltux <classicaltuxedogato@gmail.com>
2025-03-17 18:57:42 -0400

    add a and d rotation

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def main():
     clock = pygame.time.Clock()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -20,8 +21,9 @@ def main():
     
         screen.fill("black")
         player.draw(screen)
-
+        player.update(dt)
         pygame.display.flip()
+
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
