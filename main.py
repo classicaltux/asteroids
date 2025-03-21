df62333c55fe2e0f75caa560ecffeacfafa80e09
classicaltux <classicaltuxedogato@gmail.com>
2025-03-20 20:08:13 -0500

    Finish up

--- main.py
+++ main.py
@@ -24,8 +24,6 @@ def main():
 
 
     Player.containers = (updatable, drawable)
-
-
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     dt = 0
@@ -45,7 +43,8 @@ def main():
         for asteroid in asteroids:
             for shot in shots:
                 if asteroid.collides_with(shot):
-                    asteroid.kill()
+                    shot.kill()
+                    asteroid.split()
 
         screen.fill("black")
 
